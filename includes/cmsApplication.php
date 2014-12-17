32b7cfdd001f80044987df9c80cc486ca441b5da
unknown <raul.muser@KPL-A411-17.khk.local>
2014-12-17 14:41:29 +0200

    Add files school

---
+++ includes/cmsApplication.php
@@ -0,0 +1,14 @@
+<?php
+require_once('cmsBase.php');
+class CmsApplication extends CmsBase{
+    function addcontent(){
+        echo "Add";
+    }
+    function viewcontent(){
+        echo "view";
+    }
+    function anyothertask(){
+        echo "other tasks";
+    }
+}
+?>
---
+++ includes/templateFunctions.php
@@ -0,0 +1,6 @@
+<?php 
+require_once('cmsBase.php');
+class TemplateFunctions extends CmsBase{
+
+}
+?>
